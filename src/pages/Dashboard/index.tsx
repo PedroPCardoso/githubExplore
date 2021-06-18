3bbeb709ec84b9b75ad1c4e3042b61d7794d11bf
Pedro Cardoso <pedroecomp@gmail.com>
2021-06-18 10:43:46 -0300

    Adicionando Css do dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,11 +1,65 @@
 import React from 'react';
-import { Title } from './styles';
+import { Title ,Form ,Repositories} from './styles';
+import {FiChevronRight} from 'react-icons/fi';
 
+import logoImg from '../../assets/logo.svg';
 
 const Dashboard:React.FC = () => {
 
 
-    return <Title>Explore repositorios no Github </Title>
+    return(
+        <>
+        <img src={logoImg} alt="Github Explore"/>
+        <Title>Explore repositorios no Github </Title>
+        <Form action="">
+            
+            <input placeholder= "Digite o nome do repositório"></input>
+            <button type="submit"> Pesquisar </button>
+            </Form>
+
+        <Repositories>
+        <a href="teste">
+            <img
+            src="https://avatars.githubusercontent.com/u/7657883?v=4"
+            alt="Pedro Cardoso"
+            />
+            <div>
+                <strong>Template Absam</strong>
+                <p>Template de exemplo para github actions </p>
+            </div>
+            <FiChevronRight size={20} />
+
+        </a>
+
+              <a href="teste">
+            <img
+            src="https://avatars.githubusercontent.com/u/7657883?v=4"
+            alt="Pedro Cardoso"
+            />
+            <div>
+                <strong>Template Absam</strong>
+                <p>Template de exemplo para github actions </p>
+            </div>
+            <FiChevronRight size={20} />
+
+        </a>
+        
+              <a href="teste">
+            <img
+            src="https://avatars.githubusercontent.com/u/7657883?v=4"
+            alt="Pedro Cardoso"
+            />
+            <div>
+                <strong>Template Absam</strong>
+                <p>Template de exemplo para github actions </p>
+            </div>
+            <FiChevronRight size={20} />
+
+        </a>
+        </Repositories>
+        
+        </>
+    ) 
 
 };
 
