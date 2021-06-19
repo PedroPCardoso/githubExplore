1610ee99c78d4539584748a3399f269cc73b8739
Pedro Cardoso <pedroecomp@gmail.com>
2021-06-19 13:28:12 -0300

    capturando a url do repositorio e adicionando o link para as rotas

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,7 @@
-import React,{useState,FormEvent} from 'react';
+import React,{useState,FormEvent, useEffect} from 'react';
 import { Title ,Form ,Repositories,Error} from './styles';
 import {FiChevronRight} from 'react-icons/fi';
-
+import {Link} from 'react-router-dom';
 import logoImg from '../../assets/logo.svg';
 import api from'../../services/api';
 import Repository from '../Repository';
@@ -22,7 +22,25 @@ const Dashboard:React.FC = () => {
     const [newRepo,setNewRepo] = useState('');
     const [inputError,setInputError] = useState('');
 
-    const [repositories,setRepoitories] = useState<Repository[]>([]);
+ const [repositories, setRepositories] = useState<Repository[]>(() => {
+    const storageRepositories = localStorage.getItem(
+      '@GithubExplorer:repositories',
+    );
+
+    if (storageRepositories) {
+      return JSON.parse(storageRepositories);
+    }
+
+    return [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem(
+      '@GithubExplorer:repositories',
+      JSON.stringify(repositories),
+    );
+  }, [repositories]);
+
     async function handleAddRepository(event: FormEvent<HTMLFormElement>): Promise<void>{
         //adição de um novo repositório
         event.preventDefault();
@@ -36,7 +54,7 @@ const Dashboard:React.FC = () => {
             console.log(response.data);
             const repository = response.data;
     
-            setRepoitories([... repositories,repository]);
+            setRepositories([... repositories,repository]);
             setNewRepo('');
             setInputError('');
 
@@ -62,7 +80,8 @@ const Dashboard:React.FC = () => {
 
         <Repositories>      
         {repositories.map(repository => (
-        <a key={repository.full_name} href="teste">
+        <Link key={repository.full_name} 
+        to={`/respository/${repository.full_name}`}>
             <img
             src={repository.owner.avatar_url}
             alt={repository.owner.login}
@@ -73,7 +92,7 @@ const Dashboard:React.FC = () => {
             </div>
             <FiChevronRight size={20} />
 
-        </a>
+        </Link>
 
         ))}
        </Repositories>
